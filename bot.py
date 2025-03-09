b1261cd9328dd97f615517dc1252ac1ac0a757cd
qpqpalzmsk <yone135816@gmail.com>
2025-03-09 11:59:40 +0000

    ping

--- bot.py
+++ bot.py
@@ -87,7 +87,7 @@ if conn:
 # TronPy 클라이언트 초기화
 session = requests.Session()
 session.headers.update({"TRON-PRO-API-KEY": TRON_API_KEY})
-client = Tron(provider=HTTPProvider(TRON_API, client=session))
+client = Tron(provider=HTTPProvider(TRON_API, session=session))
 
 # 로깅 설정
 logging.basicConfig(
